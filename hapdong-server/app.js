be1c7af72b54352ba9aea09f989197d85a8ddf13
chaeyeonhwang <victory6309@gmail.com>
2019-11-23 17:27:55 +0900

    실시간 게시판 완료

--- hapdong-server/app.js
+++ hapdong-server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var authRouter = require('./routes/auth');
-
+const boardsRouter = require('./routes/boards')
 var app = express();
 
 // view engine setup
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/auth', authRouter);
+app.use('/boards', boardsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
