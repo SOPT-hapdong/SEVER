613e2c1b02d613c4c4190e3f95c9ecc4797e3d68
2Re-play <wogusduddnjs@gmail.com>
2019-11-23 17:14:20 +0900

    api update

--- hapdong-server/routes/auth.js
+++ hapdong-server/routes/auth.js
@@ -15,18 +15,16 @@ router.get('/users/info', (req, res) => {
       "user_univ": "솝트대 19학번",
       "posting_count": 21,
       "comment_count": 21,
-      "scrap_count": 21,
+      "scrap_count": 1,
       "user_image": "https://sopt-server.s3.ap-northeast-2.amazonaws.com/1573884765891.png"
   }
 
   try {
-
       if(!result) {
-          res.status(statusCode.BAD_REQUEST).send(util.successFalse(responseMessage.FAIL_USER,statusCode.BAD_REQUEST))
+          res.status(statusCode.BAD_REQUEST).send(util.successFalse(responseMessage.FAIL_USER, statusCode.BAD_REQUEST))
       } else {
           res.status(statusCode.OK).send(util.successTrue(responseMessage.SUCCESS_USER, result, statusCode.OK))
       }
-
   } catch (e) {
       console.log(e.message);
   }
